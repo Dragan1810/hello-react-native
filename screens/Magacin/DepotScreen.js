c04b4bfccaf69b9f7f652f16b092ee64b94bfce4
Unknown <vladimir_perisic@ymail.com>
2017-11-03 15:37:39 +0100

    dodati searchbar i back dugme

--- screens/Magacin/DepotScreen.js
+++ screens/Magacin/DepotScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
-import { Card, List, ListItem } from 'react-native-elements';
+import { Card, List, ListItem, SearchBar, Icon } from 'react-native-elements';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AppHeader from '../../components/AppHeader';
 import AppSearch from '../../components/AppSearch';
 import DepotItem from '../../components/DepotItem';
-import { getData } from '../../helpers/index';
+import { getData, filterData } from '../../helpers/index';
 import { data } from '../../helpers/Data';
 
 
@@ -14,28 +14,63 @@ class DepotScreen extends Component {
     super();
 
     this.state = {
-      data:[]
+      data:[],
+      search: ''
     };
+
+    this.seach = this.search.bind(this);
   }
 
   componentDidMount() {
     getData(data.magacin.depot).then(data => this.setState({ data }));
   }
 
+  search(e) {
+    this.setState({search:e.nativeEvent.text})
+  }
+
+
+
         render() {
+          let data = this.state.data;
+          if (this.state.data.length>1){
+            const filteredData = filterData(this.state.data, this.state.search);
+            data = filteredData;
+          }
           const { width, height } = Dimensions.get('window');
-          const { navigate } = this.props.navigation;
+          const { navigate, goBack } = this.props.navigation;
           const { text, searchBar } = styles;
             return (
+
+          <View style={styles.container}>
+          <View style={{flexDirection:'row'}}>
+            <Icon
+              containerStyle={{width:width*(1/3),alignSelf:'flex-start',margin:0,padding:0,height:30}}
+              name='arrow-left'
+              type='font-awesome'
+              color='#517fa4'
+              onPress={()=>goBack()}
+            />
+            <SearchBar
+              containerStyle={{width:width*(2/3),alignSelf:'flex-end',margin:0,padding:0}}
+              round
+              lightTheme
+              onSubmitEditing={e=>this.search(e)}
+              placeholder='Type Here...'
+            />
+            
+            </View>
+
               <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
               <FlatList
-                data={this.state.data}
+                data={data}
                 renderItem={({ item }) => (
                   <DepotItem data={item} />
                 )}
                 keyExtractor={item => item.Id}
               />
             </List>
+            </View>
 
           );
         }
@@ -45,6 +80,11 @@ const styles = StyleSheet.create({
     icon: {
       width: 24,
       height: 24,
+    },
+    container: {
+      flex: 1,
+      backgroundColor:'#C7BE9F',
+      paddingTop: 20
     },
     searchBar: {
       alignSelf: 'flex-start',
--- screens/PoslovniPartner/BusinessPartnerScreen.js
+++ screens/PoslovniPartner/BusinessPartnerScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
-import { Card, List, ListItem } from 'react-native-elements';
+import { Card, List, ListItem, SearchBar, Icon } from 'react-native-elements';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AppHeader from '../../components/AppHeader';
 import AppSearch from '../../components/AppSearch';
 import BPItem from '../../components/BPItem';
-import { getData } from '../../helpers/index';
+import { getData, filterData } from '../../helpers/index';
 import { data } from '../../helpers/Data';
 
 
@@ -14,19 +14,49 @@ class BusinessPartnerScreen extends Component {
     super(props);
 
     this.state = {
-      data:[]
+      data:[],
+      search: ''
     };
+
+    this.search = this.search.bind(this);
   }
 
   componentDidMount() {
     getData(data.poslovniPartner.businessPartner).then(data => this.setState({ data }));
   }
+  search(e) {
+    this.setState({search:e.nativeEvent.text})
+  }
 
         render() {
+          let data = this.state.data;
+          if (this.state.data.length>1){
+            const filteredData = filterData(this.state.data, this.state.search);
+            data = filteredData;
+          }
           const { width, height } = Dimensions.get('window');
-          const { navigate } = this.props.navigation;
+          const { navigate, goBack } = this.props.navigation;
           const { text, searchBar } = styles;
             return (
+              <View style={styles.container}>
+              <View style={{flexDirection:'row'}}>
+                <Icon
+                  containerStyle={{width:width*(1/3),alignSelf:'flex-start',margin:0,padding:0,height:30}}
+                  name='arrow-left'
+                  type='font-awesome'
+                  color='#517fa4'
+                  onPress={()=>goBack()}
+                />
+                <SearchBar
+                  containerStyle={{width:width*(2/3),alignSelf:'flex-end',margin:0,padding:0}}
+                  round
+                  lightTheme
+                  onSubmitEditing={e=>this.search(e)}
+                  placeholder='Type Here...'
+                />
+                
+                </View>
+                
               <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
               <FlatList
                 data={this.state.data}
@@ -36,6 +66,7 @@ class BusinessPartnerScreen extends Component {
                 keyExtractor={item => item.Id}
               />
             </List>
+            </View>
 
           );
         }
@@ -45,6 +76,11 @@ const styles = StyleSheet.create({
     icon: {
       width: 24,
       height: 24,
+    },
+    container: {
+      flex: 1,
+      backgroundColor:'#C7BE9F',
+      paddingTop: 20
     },
     searchBar: {
       alignSelf: 'flex-start',
