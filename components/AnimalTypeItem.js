a7a2acdbe3702698e1538e4454a1b7443ad3907f
Dragan <d.novakovic1810@yahoo.com>
2018-01-19 00:38:48 +0100

    Zivotinje konacno fix

--- components/AnimalTypeItem.js
+++ components/AnimalTypeItem.js
@@ -3,41 +3,47 @@ import { StyleSheet, Text, FlatList, View, Dimensions } from 'react-native';
 import { Card, List, ListItem } from 'react-native-elements';
 import { getData } from '../helpers/index'
 
-
 class AnimalTypeItem extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            data: [],
-            Id: this.props.data.Id
+            Id: this.props.data.Id,
+            data: []
         }
     }
     componentDidMount(){
         getData(`http://212.200.54.246:5001/api/AnimalSubType/GetAnimalSubTypes?CompanyId=1&AnimalTypeId=${this.state.Id}`)
-            .then(data => this.setState({ data }))
-    }
-    render(){
-        console.log(this.state.Id)
-        const { basic } = styles
-            return (
-                <Card title={this.props.data.Name}>
-            {this.state.data.length>1 && this.state.data.map((item, i) =>
-                <ListItem
-                    title={
-                        <View style={basic}>
-                            <Text>{item.Name}</Text>
-                            <Text>{item.Code}</Text>
-                        </View>
-                    }
-                    key={i}
-                    hideChevron={true}
-                />
-            )}
+        .then(data=>this.setState({data : data.AnimalSubTypes}))
 
-        </Card>
-    )
     }
-}
+
+
+    render() {
+        const { basic } = styles
+        let { data } = this.state
+
+        return (
+               <Card title={this.props.data.Name}>
+                    {data.length>1 && data.map((item, i) => (
+                                        <ListItem
+                                            key={i}
+                                            hideChevron={true}
+                                            title={
+                                                <View style={basic}>
+                                                    <Text>{item.Name}</Text>
+                                                    <Text>{item.Code}</Text>
+
+                                                </View>
+                                            }
+                                        />
+                                    ))}
+               </Card>
+            )
+    }
+
+
+
+    }
 
 const styles = StyleSheet.create({
     basic: {
@@ -48,12 +54,16 @@ const styles = StyleSheet.create({
 export default AnimalTypeItem;
 
 /*
-"Id": 9,
-"Code": 1002,
-"CompanyId": 22,
-"Name": "Depo 2",
-"Description": null,
-"Boxes": null,
-"CreatedById": 13,
-"CreatedByName": "Ivan Ivanovic"
+  {Data.AnimalSubTypes.map((item, i) => (
+                    <ListItem
+                        key={i}
+                        title={
+                            <View>
+                                <Text>{item.Name}</Text>
+                                <Text>{item.Code}</Text>
+
+                            </View>
+                        }
+                    />
+                ))}
 */
