047f2f7569b42ccb35b20ea197d783698012f621
Dragan <d.novakovic1810@yahoo.com>
2018-03-14 07:27:47 +0100

    button fix

--- components/PredhodnaKlanja/PreLambKlanja.js
+++ components/PredhodnaKlanja/PreLambKlanja.js
@@ -1,61 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, FlatList, View, Dimensions } from 'react-native';
-import { Card, List, ListItem, Button } from 'react-native-elements';
-import { format } from 'date-fns'
+import React from "react";
+import { StyleSheet, Text, FlatList, View, Dimensions } from "react-native";
+import { Card, List, ListItem, Button } from "react-native-elements";
+import { format } from "date-fns";
 
-
-
-const ListItems = ({data, navigate}) => {
-    const { basic } = styles
-    return(
-        <Card title={data.Supplier.Name}>
-        <ListItem
-            title={
-                <View style={basic}>
-                    <Text>Datum klanja:</Text>
-                    <Text>{format(data.LambSlaughterDate, 'DD/MM/YYYY')}</Text>
-                </View>
-            }
-            hideChevron={true}
-        />
-        <ListItem
-            title={
-            <View style={basic}>
-                <Text>Cena po Kg:</Text>
-                <Text>{(data.BuyingPrice/data.WeightAfterLambSlaughter).toFixed(4)}</Text>
-            </View>
-            }
-            hideChevron={true}
-        />
-        <ListItem
-            title={
-            <View style={basic}>
-                <Text>Randman:</Text>
-                <Text>{data.Randman}</Text>
-            </View>
-            }
-            hideChevron={true}
-        />
-        <Button
-            containerViewStyle={{paddingTop: 12}}
-            icon={{name: 'code'}}
-            onPress={() => navigate('details', { url : `http://212.200.54.246:5001/api/LambSlaughter/GetAllBySlaughter?SlaughterId=${data.Id}` })}
-            backgroundColor='#009688'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='Detalji'
-        />
-
+const ListItems = ({ data, navigate }) => {
+  const { basic } = styles;
+  return (
+    <Card title={data.Supplier.Name}>
+      <ListItem
+        title={
+          <View style={basic}>
+            <Text>Datum klanja:</Text>
+            <Text>{format(data.LambSlaughterDate, "DD/MM/YYYY")}</Text>
+          </View>
+        }
+        hideChevron={true}
+      />
+      <ListItem
+        title={
+          <View style={basic}>
+            <Text>Cena po Kg:</Text>
+            <Text>
+              {(data.BuyingPrice / data.WeightAfterLambSlaughter).toFixed(4)}
+            </Text>
+          </View>
+        }
+        hideChevron={true}
+      />
+      <ListItem
+        title={
+          <View style={basic}>
+            <Text>Randman:</Text>
+            <Text>{data.Randman}</Text>
+          </View>
+        }
+        hideChevron={true}
+      />
+      <Button
+        containerViewStyle={{ paddingTop: 12 }}
+        onPress={() =>
+          navigate("details", {
+            url: `http://212.200.54.246:5001/api/LambSlaughter/GetAllBySlaughter?SlaughterId=${
+              data.Id
+            }`
+          })
+        }
+        backgroundColor="#009688"
+        buttonStyle={{
+          borderRadius: 0,
+          marginLeft: 0,
+          marginRight: 0,
+          marginBottom: 0
+        }}
+        title="Detalji"
+      />
     </Card>
-    )
-}
+  );
+};
 export default ListItems;
 
 const styles = StyleSheet.create({
-    basic: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
-})
+  basic: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
+});
 /*
 •	OrderNumber - Br. klanja
 •	ProductName - Artikal
