3bb336ceabcf172cd029ac1fe82393ba555643c3
Dragan <d.novakovic1810@yahoo.com>
2018-04-20 21:03:47 +0200

    Updates

--- components/Web.js
+++ components/Web.js
@@ -1,64 +1,32 @@
-import React, { Component } from 'react'
-import { WrapperHeader, Wrapper, TitleText } from '../styled-components/Wrapper'
-import { Card, List, ListItem, SearchBar, Icon } from 'react-native-elements';
+import React, { Component } from "react";
 import {
-   View,
-   WebView,
-   StyleSheet,
-   Dimensions,
-   Text
-}
-from 'react-native'
-const {height: Dheight, width: Dwidth } = Dimensions.get('window')
+  WrapperHeader,
+  Wrapper,
+  TitleText
+} from "../styled-components/Wrapper";
+import { Card, List, ListItem, SearchBar, Icon } from "react-native-elements";
+import { View, WebView, StyleSheet, Dimensions, Text } from "react-native";
+import Header from "../reusable-components/Header";
 
+const { height: Dheight, width: Dwidth } = Dimensions.get("window");
 
-const WebViewExample = ({ navigation }) => {
-      const { search, icon, video } = styles
-      const { uri, ime } = navigation.state.params
-   return (
-      <Wrapper>
-      <WrapperHeader>
-        <Icon
-          containerStyle={icon}
-          name='chevron-left'
-          type='font-awesome'
-          color='#fff'
-          size={32}
-          onPress={()=> navigation.goBack()}
-        />
-        <TitleText style={search}>{ime}</TitleText>
-          </WrapperHeader>
-         <View style = {{flex:1}}>
-         <WebView
-            style={video}
-            source = {{ uri }}
-         />
+const WebViewExample = ({ navigation: { goBack, navigate } }) => {
+  const { video } = styles;
+  const { uri, ime } = navigation.state.params;
+  return (
+    <Wrapper>
+      <Header title={ime} navigate={navigate} goBack={goBack} />
+      <View style={{ flex: 1 }}>
+        <WebView style={video} source={{ uri }} />
       </View>
-        </Wrapper>
-
-
-   )
-}
+    </Wrapper>
+  );
+};
 const styles = StyleSheet.create({
-      icon: {
-        flex: 1,
-        alignSelf:'flex-start',
-        margin:0,
-        padding:0,
-        backgroundColor:'#009688',
-        height: 56
-      },
-      video: {
-            height: Dheight,
-            width: Dwidth,
-            flex: 1
-          },
-      search: {
-        flex: 3,
-        alignSelf:'center',
-        justifyContent: 'center',
-        color: 'white',
-        paddingLeft: 20
-      }
-    });
+  video: {
+    height: Dheight,
+    width: Dwidth,
+    flex: 1
+  }
+});
 export default WebViewExample;
